102e2e851139c3803c1e0eead1cdce52e2b908cc
existanza <kraft.sc2@gmail.com>
2016-04-13 19:33:22 +0200

    23th commit

--- dd3.py
+++ dd3.py
@@ -4,6 +4,8 @@
 # 3.4.3 (default, Oct 14 2015, 20:28:29)
 # [GCC 4.8.4]
 
+# TODO - threegrams?
+
 import sys
 import time
 import random
@@ -27,8 +29,7 @@ capitalsList = [c for c in capitals]
 toSmallDict = {c: s for c, s in zip(capitals, alphabet)}
 aDict = {ch: i for ch, i in zip(alphabet, range(len(alphabet) + 1))}
 twoGramMatrix = [[0] * aSize for i in range(aSize)]
-threeGramMatrix = [[[0] * aSize for i in range(aSize)] *
-                   aSize for j in range(aSize)]
+threeGramMatrix = [[[0] * aSize for i in range(aSize)] for j in range(aSize)]
 characters = [0] * 21
 characters2 = [0] * 21
 singletons = [0] * aSize
@@ -55,7 +56,7 @@ def analyze_n_grams(w):
     for i in range(2, len(w)):
         twoGramMatrix[aDict[w[i]]][aDict[w[i - 1]]] += 1
         twoGramMatrix[aDict[w[i]]][0] += 1
-        threeGramMatrix[aDict[w[i]]][aDict[w[i - 1]]][aDict[w[i - 2]]] = True
+        threeGramMatrix[aDict[w[i]]][aDict[w[i - 1]]][aDict[w[i - 2]]] += 1
 
 
 def complete_bigram(ch):
@@ -71,7 +72,7 @@ def complete_bigram(ch):
 def sanitize(w):
     w = '^' + w + '$'
     for i in range(2, len(w)):
-        if not threeGramMatrix[aDict[w[i]]][aDict[w[i - 1]]][aDict[w[i - 2]]]:
+        if threeGramMatrix[aDict[w[i]]][aDict[w[i - 1]]][aDict[w[i - 2]]] == 0:
             return False
     return True
 
@@ -104,6 +105,26 @@ for line in inputFile:
 
 wordsRemaining = wordCounter
 wordsToCreate = 2*wordCounter
+
+popularDict = {}
+for i in range(3, len(twoGramMatrix)):
+    for j in range(3, len(twoGramMatrix[i])):
+        if twoGramMatrix[i][j] > charCounter/100:
+            popularDict[alphabet[j] + alphabet[i]] = twoGramMatrix[i][j]/charCounter*100
+
+for el in reversed(sorted(popularDict.items(), key=lambda x: x[1])):
+    print(el)
+
+popularDict3 = {}
+for i in range(1, len(threeGramMatrix)):
+    for j in range(1, len(threeGramMatrix[i])):
+        for k in range(1, len(threeGramMatrix[j])):
+            if threeGramMatrix[i][j][k] > charCounter/200:
+                popularDict3[alphabet[k] + alphabet[j] + alphabet[i]] = threeGramMatrix[i][j][k]/charCounter*100
+
+for el in reversed(sorted(popularDict3.items(), key=lambda x: x[1])):
+    print(el)
+
 
 printf("Creating words")
 
